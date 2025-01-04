cb57d90dcd66accb4cc375dff55de16524d3a54e
chadsteele <chad@chadsteele.com>
2025-01-04 19:41:43 +0400

    improved, fixed bugs

---
+++ popup.js
@@ -0,0 +1,27 @@
+document.getElementById("saveButton").addEventListener("click", () => {
+	const maxTime = parseInt(document.getElementById("maxTime").value)
+	const transitionTime = parseInt(
+		document.getElementById("transitionTime").value
+	)
+	const enabled = document.getElementById("enabled").checked
+
+	const config = {
+		maxTime,
+		transitionTime,
+		enabled,
+	}
+
+	chrome.storage.local.set({config}, () => {
+		console.log("Settings saved!")
+	})
+})
+
+// Load saved settings
+chrome.storage.local.get(["config"], (result) => {
+	if (result.config) {
+		document.getElementById("maxTime").value = result.config.maxTime
+		document.getElementById("transitionTime").value =
+			result.config.transitionTime
+		document.getElementById("enabled").checked = result.config.enabled
+	}
+})
